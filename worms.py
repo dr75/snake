c117faefecfdbc8a02c3d859c77b35c38283b072
Marko Rosenmueller <marko@rosenmueller.de>
2022-03-29 11:42:18 +0200

    comment out

--- worms.py
+++ worms.py
@@ -3,9 +3,9 @@ from terminal import print_at, wait_key, term_height, clear_screen
 
 def main():
     clear_screen()
-    height = term_height()
-    print_at(0, height - 2, "> hello...")
-    print_at(0, height - 1, "> good bye...")
+    # height = term_height()
+    # print_at(0, height - 2, "> hello...")
+    # print_at(0, height - 1, "> good bye...")
 
 
 if __name__=='__main__':
